527036810c9de4366a46a47f60973eb4aa7e5f03
BranfordTGbieor <gbieorbranford@gmail.com>
2019-04-19 04:28:43 +0100

    commit-1

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const mongoose = require('mongoose');
 
 const genre = require('./routes/genre');
 const customers = require('./routes/customers');
 const movie = require('./routes/movies');
+const rental = require('./routes/rentals');
 
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('connected to the database'))
@@ -14,6 +17,7 @@ app.use(express.json());  //express built-in middle-wares
 app.use('/api/genres', genre);
 app.use('/api/customers', customers);
 app.use('/api/movies', movie);
+app.use('/api/rentals', rental);
 
 
 //API port configuration
