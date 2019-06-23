6455b2a5da22cf1e2eb975870e960a1bcd28e419
BranfordTGbieor <gbieorbranford@gmail.com>
2019-06-23 20:03:33 +0100

    adjusted routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -28,10 +28,10 @@ router.post('/', async (req, res) => {
     const { error } = validateUser(req.body);
      if(error) return res.status(400).send(error.details[0].message);
 
-     let user = await User.findOne({ email:req.body.email });
+     const user = await User.findOne({ email:req.body.email });
      if (user) return res.status(400).send('user already exist');
 
-    let user = new User({ 
+       user = new User({ 
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
